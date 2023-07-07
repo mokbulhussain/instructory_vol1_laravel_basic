8416180a5f79b455db51655fe7f17e6fbed3b62a
mokbulhussain <mokbulhussain2@gmail.com>
2023-07-07 09:22:40 +0600

    database setup ar jonno .env file use korte hobe

---
+++ documentation.php
@@ -0,0 +1,11 @@
+<?php 
+/*
+
+// database setup ar jonno .env file use korte hobe
+
+DB_CONNECTION=mysql
+DB_HOST=127.0.0.1
+DB_PORT=3306
+DB_DATABASE=ins_vol1_basic
+DB_USERNAME=root
+DB_PASSWORD=
