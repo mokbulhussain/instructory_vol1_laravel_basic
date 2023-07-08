6b1bfce003b669f27c37945f3a4a6bf13417e310
mokbulhussain <mokbulhussain2@gmail.com>
2023-07-08 13:58:00 +0600

    route parameter with regular expression

--- routes/web.php
+++ routes/web.php
@@ -44,8 +44,29 @@ Route::get('/contact',function(){
 
 
 // optional parameter with route
-Route::get('/service/{service_id}/{service_name?}',function($service_id,$service_name=null){
-    return 'service'.$service_id.$service_name;
-})->name('service');
+// Route::get('/service/{service_id}/{service_name?}',function($service_id,$service_name=null){
+//     return 'service'.$service_id.$service_name;
+// })->name('service');
 
 
+
+
+// route parameter with regular expression & one parameter
+// Route::get('/user/{name}',function($name){
+//     echo $name;
+// })->where('name','[A-Za-z]+');
+
+
+
+// route parameter with regular expression & multiple parameter
+Route::get('/user/{id}/{name}',function($id,$name){
+    echo $id,$name;
+})->where(['id'=>'[0-9]+', 'name'=>'[a-z]+']);
+
+
+
+
+// route parameter with regular expression & parameter check with array multiple value.
+Route::get('/category/{category_name}',function($category_name){
+    echo $category_name;
+})->whereIn('category_name',['electornics','movie','books']);
