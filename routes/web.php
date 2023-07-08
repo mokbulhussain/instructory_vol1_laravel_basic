1ac30a2b74cc38eb59ae3b6477b7144870ca2428
mokbulhussain <mokbulhussain2@gmail.com>
2023-07-08 13:27:44 +0600

    route parameter & multipule parameter & optional parameter

--- routes/web.php
+++ routes/web.php
@@ -22,10 +22,30 @@ Route::get('/about',function(){
     return view("about");
 })->name('about');
 
-Route::get('/service',function(){
-    return view('service');
-})->name('service');
+// Route::get('/service',function(){
+//     return view('service');
+// })->name('service');
 
 Route::get('/contact',function(){
     return view('contact');
 })->name('contact');
+
+
+// parameter with route
+// Route::get('/service/{service_id}',function($service_id){
+//     return 'service'.$service_id;
+// })->name('service');
+
+
+// multipule parameter with route
+// Route::get('/service/{service_id}/{service_name}',function($service_id,$service_name){
+//     return 'service'.$service_id.$service_name;
+// })->name('service');
+
+
+// optional parameter with route
+Route::get('/service/{service_id}/{service_name?}',function($service_id,$service_name=null){
+    return 'service'.$service_id.$service_name;
+})->name('service');
+
+
