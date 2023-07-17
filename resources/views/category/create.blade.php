19c15042931d109af238114f438c790967e1a186
mokbulhussain <mokbulhussain2@gmail.com>
2023-07-17 10:58:31 +0600

    showing validation error message

--- resources/views/category/create.blade.php
+++ resources/views/category/create.blade.php
@@ -7,7 +7,15 @@
         @csrf
         <div class="mb-3">
           <label for="exampleInputEmail1" class="form-label">Category</label>
-          <input type="text" name="category_name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+          <input type="text" name="category_name" class="form-control @error('category_name')
+            is-invalid
+          @enderror" id="exampleInputEmail1" aria-describedby="emailHelp">
+
+          @error('category_name')
+            <span class="invalid-feedback" role="alert">
+              <strong>{{$message}}</strong>
+            </span>
+          @enderror
         </div>
 
         <div class="mb-3">
