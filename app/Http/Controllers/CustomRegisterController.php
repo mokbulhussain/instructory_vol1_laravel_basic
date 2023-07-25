1dfa8dd5e77e167d4e16d5063c686c7c3144fe8e
mokbulhussain <mokbulhussain2@gmail.com>
2023-07-25 11:04:40 +0600

    custome register with valitation

--- app/Http/Controllers/CustomRegisterController.php
+++ app/Http/Controllers/CustomRegisterController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Requests\RegisterStoreRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class CustomRegisterController extends Controller
 {
@@ -21,5 +23,20 @@ class CustomRegisterController extends Controller
             'phone'=>$request->phone,
             'password'=>Hash::make($request->password)
         ]);
+
+
+        $credentials=[
+            'email' =>$request->email,
+            'password'=>$request->password
+        ];
+
+        if(Auth::attempt($credentials)){
+            $request->session()->regenerate();
+            return redirect()->intended('home');
+        }
+
+        return back()->withErrors([
+            'email'=>'Wrong creadential found'
+        ])->onlyInput('email');
     }
 }
